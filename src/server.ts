7d710e0e6a79df6a82bfa798068f51119470fe73
Guilherme <guilherme.llopreti@gmail.com>
2022-07-26 18:37:12 -0300

    build: initial configs

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import { AppDataSource } from "./data-source";
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source Initialized");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source Initialization");
+    });
+  console.log(`app running on port ${port}`);
+});
