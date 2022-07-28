165ea8f31791e16e0703f845718a98a4d58e20c2
Guilherme <guilherme.llopreti@gmail.com>
2022-07-28 14:38:48 -0300

    build: tests configs

---
+++ src/__tests__/test.spec.ts
@@ -0,0 +1,6 @@
+describe("Create an user", () => {
+  test("Should insert the information of the new user in the database", () => {
+    const soma = 2 + 2;
+    expect(soma).toEqual(4);
+  });
+});
