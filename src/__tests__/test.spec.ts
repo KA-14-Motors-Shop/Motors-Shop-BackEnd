7bedb6ca7f443ed4411e3ff1866462927165d693
Guilherme <guilherme.llopreti@gmail.com>
2022-07-28 15:20:56 -0300

    fix: test example

--- src/__tests__/test.spec.ts
+++
@@ -1,6 +0,0 @@
-describe("Create an user", () => {
-  test("Should insert the information of the new user in the database", () => {
-    const soma = 2 + 2;
-    expect(soma).toEqual(4);
-  });
-});
