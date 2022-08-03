e6c9cd1c628acadc6d254bd5e1ce75a249b0a849
Guilherme <guilherme.llopreti@gmail.com>
2022-08-03 20:02:36 -0300

    feat: create tables and relations entities and migrations

---
+++ src/entities/images.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Advertisement } from "./advertisements.entity";
+
+@Entity()
+export class Image {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column({ type: "varchar", width: 1500, nullable: false })
+  url: string;
+
+  @CreateDateColumn({
+    name: "created_at",
+    type: "timestamp",
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: "updated_at",
+    type: "timestamp",
+  })
+  updatedAt: Date;
+
+  @ManyToOne((type) => Advertisement, (advertisement) => advertisement.images, {
+    onDelete: "CASCADE",
+  })
+  advertisement: Advertisement;
+}
---
+++ src/entities/users.entity.ts
@@ -0,0 +1,71 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+import { Address } from "./addresses.entity";
+import { Advertisement } from "./advertisements.entity";
+
+export enum UserType {
+  BUYER = "buyer",
+  ADVERTISER = "advertiser",
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column({ type: "varchar", width: 256, nullable: false })
+  name: string;
+
+  @Column({ type: "varchar", width: 256, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", width: 11, nullable: false, unique: true })
+  cpf: string;
+
+  @Column({ type: "varchar", width: 11, nullable: false, unique: true })
+  cell_phone: string;
+
+  @Column({ type: "date" })
+  birthday: string;
+
+  @Column({ type: "varchar", width: 256, nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", width: 500 })
+  description: string;
+
+  @Column({ type: "enum", enum: UserType, default: UserType.BUYER })
+  type: UserType;
+
+  @Column({ type: "boolean", nullable: false })
+  is_active: boolean;
+
+  @CreateDateColumn({
+    name: "created_at",
+    type: "timestamp",
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: "updated_at",
+    type: "timestamp",
+  })
+  updatedAt: Date;
+
+  @OneToOne((type) => Address, { eager: true })
+  @JoinColumn()
+  address: Address;
+
+  @OneToMany((type) => Advertisement, (advertisement) => advertisement.owner, {
+    eager: true,
+  })
+  advertisements: Advertisement[];
+}
