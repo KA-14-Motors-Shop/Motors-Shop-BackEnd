6669b247df6910496bf329474f9bc0971eb62e49
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-08-10 21:29:41 -0300

    feat: create ad test enhancement

--- src/__tests__/createAd.service.spec.ts
+++ src/__tests__/createAd.service.spec.ts
@@ -6,6 +6,7 @@ import {
 } from "../entities/advertisements.entity";
 import { Image } from "../entities/images.entity";
 import CreateAdvertisementService from "../services/advertisement/CreateAdvertisement.service";
+import UserCreateService from "../services/user/userCreate.service";
 
 describe("Create an advertisement", () => {
   let connection: DataSource;
@@ -23,6 +24,23 @@ describe("Create an advertisement", () => {
   });
 
   test("Should insert the new ad in the database", async () => {
+    const owner = await UserCreateService.creationService({
+      name: "test",
+      cpf: "1234",
+      email: "test2@mail.com",
+      password: "1234",
+      description: "aylmao",
+      cell_phone: "12345",
+      birthday: "1999-01-01",
+      address: {
+        cep: "123456",
+        state: "teststate",
+        city: "testc",
+        street: "street",
+        number: 69,
+        complement: "ay lmao",
+      },
+    });
     const image = new Image();
     image.url = "testurl@img.com";
     const type = AdvertisementType.SALE;
@@ -34,7 +52,7 @@ describe("Create an advertisement", () => {
     const vehicle_type = VehicleType.CAR;
     const is_active = true;
     const images = [image];
-    const userEmail = "test@mail.com";
+    const userEmail = "test2@mail.com";
 
     const newAd = await CreateAdvertisementService.execute({
       type,
@@ -62,5 +80,7 @@ describe("Create an advertisement", () => {
         images,
       })
     );
+
+    expect(newAd.owner.id).toBe(owner.id);
   });
 });
