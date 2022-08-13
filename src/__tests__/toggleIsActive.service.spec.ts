922fc264410509c80582141d66bcb57c9f99f496
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-08-13 20:09:45 -0300

    feat: toggle is active service test

---
+++ src/__tests__/toggleIsActive.service.spec.ts
@@ -0,0 +1,75 @@
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../data-source";
+import {
+  AdvertisementType,
+  VehicleType,
+} from "../entities/advertisements.entity";
+import { Image } from "../entities/images.entity";
+import CreateAdvertisementService from "../services/advertisement/CreateAdvertisement.service";
+import ToggleIsActiveAdService from "../services/advertisement/ToggleIsActiveAd.service";
+import UserCreateService from "../services/user/userCreate.service";
+
+describe("Toggle an advertisement is active status", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("Should toggle is active of an ad", async () => {
+    const owner = await UserCreateService.creationService({
+      name: "test",
+      cpf: "1234560",
+      email: "test3@mail.com",
+      password: "1234",
+      description: "aylmao",
+      cell_phone: "0123456",
+      birthday: "1999-01-01",
+      address: {
+        cep: "123456",
+        state: "teststate",
+        city: "testc",
+        street: "street",
+        number: 69,
+        complement: "ay lmao",
+      },
+    });
+    const image = new Image();
+    image.url = "testurl@img.com";
+    const type = AdvertisementType.SALE;
+    const title = "test title";
+    const year = 2000;
+    const mileage = 15000;
+    const price = 30000;
+    const description = "test desc";
+    const vehicle_type = VehicleType.CAR;
+    const is_active = true;
+    const images = [image];
+    const userEmail = "test3@mail.com";
+
+    const newAd = await CreateAdvertisementService.execute({
+      type,
+      title,
+      year,
+      mileage,
+      price,
+      description,
+      vehicle_type,
+      is_active,
+      images,
+      userEmail,
+    });
+
+    expect(newAd.is_active).toBe(true);
+    const toggleAd = await ToggleIsActiveAdService.execute(newAd.id);
+    expect(toggleAd?.is_active).toBe(false);
+  });
+});
