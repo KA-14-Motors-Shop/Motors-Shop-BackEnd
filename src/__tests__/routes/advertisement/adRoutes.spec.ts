253a4bec8f7c12b98827f6820bf4128355b61a50
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-08-19 19:35:35 -0300

    feat: list ads route test

--- src/__tests__/routes/advertisement/adRoutes.spec.ts
+++ src/__tests__/routes/advertisement/adRoutes.spec.ts
@@ -1,50 +1,86 @@
 import request from "supertest";
 import app from "../../../app";
 import { AppDataSource } from "../../../data-source";
+import {
+  AdvertisementType,
+  VehicleType,
+} from "../../../entities/advertisements.entity";
+import CreateAdvertisementService from "../../../services/advertisement/CreateAdvertisement.service";
 import UserCreateService from "../../../services/user/userCreate.service";
 import UserLoginService from "../../../services/user/userLogin.service";
 
 describe("Testing ad routes", () => {
-  // beforeAll(async () => {
-  //   await AppDataSource.initialize().catch((err) => console.log(err));
-  // });
-  // afterAll(async () => {
-  //   await AppDataSource.dropDatabase();
-  //   await AppDataSource.destroy().catch((err) => console.log(err));
-  // });
+  beforeAll(async () => {
+    await AppDataSource.initialize().catch((err) => console.log(err));
+  });
+  afterAll(async () => {
+    await AppDataSource.dropDatabase();
+    await AppDataSource.destroy().catch((err) => console.log(err));
+  });
 
-  // const getOwner = async () => {
-  //   const owner = await UserCreateService.creationService({
-  //     name: "test",
-  //     cpf: "123456",
-  //     email: "test@mail.com",
-  //     password: "1234",
-  //     description: "aylmao",
-  //     cell_phone: "12345",
-  //     birthday: "1999-01-01",
-  //     address: {
-  //       cep: "123456",
-  //       state: "teststate",
-  //       city: "testc",
-  //       street: "street",
-  //       number: 69,
-  //       complement: "ay lmao",
-  //     },
-  //   });
+  const getOwner = async () => {
+    const owner = await UserCreateService.creationService({
+      name: "test",
+      cpf: "123456",
+      email: "test@mail.com",
+      password: "1234",
+      description: "aylmao",
+      cell_phone: "12345",
+      birthday: "1999-01-01",
+      address: {
+        cep: "123456",
+        state: "teststate",
+        city: "testc",
+        street: "street",
+        number: 69,
+        complement: "ay lmao",
+      },
+    });
 
-  //   return owner;
-  // };
+    return owner;
+  };
 
-  // const getToken = async () => {
-  //   const owner = getOwner();
+  const getToken = async () => {
+    const owner = getOwner();
 
-  //   const token = await UserLoginService.userLoginService({
-  //     email: (await owner).email,
-  //     password: "1234",
-  //   });
+    const token = await UserLoginService.userLoginService({
+      email: (await owner).email,
+      password: "1234",
+    });
 
-  //   return token;
-  // };
+    return token;
+  };
+
+  const createAd = async (
+    type: AdvertisementType = AdvertisementType.SALE,
+    title: string = "Test title",
+    year: number = 2000,
+    mileage: number = 0,
+    price: number = 40000,
+    description: string = "Test description",
+    vehicle_type: VehicleType = VehicleType.CAR,
+    is_active: boolean = true,
+    userEmail: string = "test@mail.com"
+  ) => {
+    const ad = await CreateAdvertisementService.execute(
+      {
+        type,
+        title,
+        year,
+        mileage,
+        price,
+        description,
+        vehicle_type,
+        is_active,
+      },
+      userEmail,
+      [
+        { url: "testurl@img.com", is_front: true },
+        { url: "testurl2@img.com", is_front: false },
+      ]
+    );
+    return ad;
+  };
 
   it("Should be able to create a new ad", async () => {
     // const token = await getToken();
@@ -71,5 +107,15 @@ describe("Testing ad routes", () => {
     //   .send(adData);
 
     expect(2 + 2).toBe(4);
+  });
+
+  it("Should be able to list all ads", async () => {
+    const owner = await getOwner();
+    const ad = await createAd();
+    const response = await request(app).get("/ads");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("map");
+    expect(response.body[0].id).toEqual(ad.id);
   });
 });
