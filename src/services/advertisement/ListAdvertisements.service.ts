10020a4fe72b010e4bfbd7a3bfe62d94036a6c31
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-08-19 22:24:11 -0300

    feat: list ad service now returns owner of each ad

--- src/services/advertisement/ListAdvertisements.service.ts
+++ src/services/advertisement/ListAdvertisements.service.ts
@@ -1,10 +1,33 @@
 import { Advertisement } from "../../entities/advertisements.entity";
 import { AppDataSource } from "../../data-source";
+import { User } from "../../entities/users.entity";
 
 export default class ListAdvertisementsService {
   static async execute() {
     const adRepo = AppDataSource.getRepository(Advertisement);
+    const userRepo = AppDataSource.getRepository(User);
+
+    const users = await userRepo.find();
     const ads = await adRepo.find();
-    return ads;
+
+    const adList = ads.map((ad) => {
+      const owner = users.find((user) =>
+        user.advertisements.some((adv) => adv.id === ad.id)
+      );
+
+      const serializedAd = {
+        ...ad,
+        owner: {
+          id: owner?.id,
+          name: owner?.name,
+          email: owner?.email,
+          type: owner?.type,
+        },
+      };
+
+      return serializedAd;
+    });
+
+    return adList;
   }
 }
